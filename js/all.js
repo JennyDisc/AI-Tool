ac32470e0461626ba80c60375a3af5e0122ae34b
JennyDisc <touki741@hotmail.com>
2023-07-05 00:46:04 +0800

    依助教建議修改

--- js/all.js
+++ js/all.js
@@ -3,23 +3,47 @@ $(document).ready(function () {
     $(".navbar-btn").click(function (e) {
         // 點擊後展開下拉選單
         $(".navbar-btn-collapse").toggleClass("show");
+        // 點擊後icon符號也要相應切換
+        $(this).find('.navbar-btn-close').toggleClass('show');
+        $(this).find('.navbar-btn-menu').toggleClass('hide');
         // 點擊後下方.headerTitle要位移(否則會被選單覆蓋)
         $(".headerTitle").toggleClass("move-bottom");
     });
 
-    // 
-    // $('.headerTitle-img').click(function (e) { 
-    //     $('img:nth-child(1)').addClass('animate_animated animate__heartBeat');
-    //     $('img:nth-child(2)').addClass('animate_animated animate__heartBeat');
-    //     $('img:nth-child(3)').addClass('animate_animated animate__heartBeat');        
-    // });
 
-    // AI工具列表選單展開
+    // AI工具列左側篩選單展開
+    $(".aiTool-filter-btn").click(function (e) {
+        // 點擊後展開下拉選單
+        $(".dropdown-btn-filter").toggleClass("show");
+    });
+    // AI工具列左側篩選單：勾選的項目右側出現v符號
+    $(".filter-content").click(function (e) {
+        e.preventDefault();
+        // 目標：自己<a>父層<li>以外，底下的<span>移除被點擊而class增加span.show，並增加d-none
+        // 消除前一次選到的<a>底下<span>的class=span.show能被消除
+        $(this).parent().siblings().find('.filter-content span').removeClass('show');
+
+        // 前一次選到的<a>底下<span>增加的class=d-none
+        $(this).parent().siblings().find('.filter-content span').addClass('d-none');
+
+        // 目標：點擊到的項目上一層<li>底下<span>，移除原有class=d-none並增加class=span.show
+        // 消除點擊到的父層底下<span>原有的class=d-none
+        $(this).parent().find('.filter-content span').removeClass('d-none');
+
+        // 點擊到的父層底下<span>增加的class=span.show
+        $(this).parent().find('.filter-content span').addClass('show');
+    });
+
+
+    // AI工具列右側篩選單展開
     $(".dropdown-btn").click(function (e) {
         // 點擊後展開下拉選單
         $(".dropdown-btn-menu").toggleClass("show");
+        // 點擊後icon符號也要相應切換
+        $(this).find('.dropdown-btn-expandLess').toggleClass('show');
+        $(this).find('.dropdown-btn-expandMore').toggleClass('hide');
     });
-    // AI工具列表選單：切換按鈕文字
+    // AI工具列右側篩選單：切換按鈕文字
     $(".new-to-old").click(function (e) {
         e.preventDefault();
         // 選完後把一開始點擊按鈕的狀態show關閉
@@ -39,10 +63,8 @@ $(document).ready(function () {
         e.preventDefault();
         $(this).toggleClass('active');
         // 展開答案
-        // $(this).toggleClass("show");
         $(this).find('.add-icon').toggleClass('show');
         $(this).find('.remove-icon').toggleClass('show');
         $(this).find('.question-block-collapse').toggleClass('show');
-        // $(".remove-icon").toggleClass("show");
     });
 });
